a49e43544e631aeab5fc340712b5111b18d397c9
Jeremy Klein <jklein24@gmail.com>
2023-12-12 23:17:23 -0800

    Validate the uma address data included in requests
    This addresses some SSRF risks associated with the demo vasp. Note that a real vasp might also do additional domain whitelisting, checks,  or validation of the vasps they communicate with.

--- examples/uma-server/vasp2.go
+++ examples/uma-server/vasp2.go
@@ -123,6 +123,14 @@ func (v *Vasp2) parseUmaQueryData(context *gin.Context) ([]byte, bool) {
 		return nil, true
 	}
 
+	vaspDomainValidationErr := ValidateDomain(query.VaspDomain)
+	if vaspDomainValidationErr != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"status": "ERROR",
+			"reason": fmt.Sprintf("Invalid sending VASP domain: %v", vaspDomainValidationErr),
+		})
+		return nil, true
+	}
 	pubKeys, err := uma.FetchPublicKeyForVasp(query.VaspDomain, v.pubKeyCache)
 	if err != nil || pubKeys == nil {
 		context.JSON(http.StatusBadRequest, gin.H{
@@ -289,7 +297,8 @@ func (v *Vasp2) handleUmaPayreq(context *gin.Context) {
 	}
 
 	sendingVaspDomain, err := uma.GetVaspDomainFromUmaAddress(request.PayerData.Identifier)
-	if err != nil {
+	addressValidationError := ValidateUmaAddress(request.PayerData.Identifier)
+	if err != nil || addressValidationError != nil {
 		context.JSON(http.StatusBadRequest, gin.H{
 			"status": "ERROR",
 			"reason": fmt.Sprintf("Invalid sender indentifier. UMA address required in the format $alice@vasp.com: %v", err),
