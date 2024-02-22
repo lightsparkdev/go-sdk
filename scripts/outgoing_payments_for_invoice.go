6aa5a06ebb6a629a1fc0315b692d720ed023890b
Zhen Lu <zhenlu@lightspark.com>
2024-02-22 14:59:00 -0800

    Fix script typo

--- scripts/outgoing_payments_for_invoice.go
+++ scripts/outgoing_payments_for_invoice.go
@@ -5,9 +5,9 @@ import "github.com/lightsparkdev/go-sdk/objects"
 const OUTGOING_PAYMENTS_FOR_INVOICE_QUERY = `
 query OutgoingPaymentsForInvoice(
 	$encoded_invoice: String!
-	$statuses: [PaymentStatus!]
-} {
-	outgoing_payments_for_invoice_query(input: {
+	$statuses: [TransactionStatus!]
+) {
+	outgoing_payments_for_invoice(input: {
 		encoded_invoice: $encoded_invoice
 		statuses: $statuses
 	}) {
