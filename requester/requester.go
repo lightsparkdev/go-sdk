balena-zh/lightspark-go-sdk

Implemented a new `ExecuteGraphqlWithContext` function so that users willing to honor a given context termination can use the Lightspark go-sdk.

requester/requester.go
@@ -3,11 +3,12 @@ package requester
 
 import (
 	"bytes"
+	"context"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
 	"errors"
-	"io/ioutil"
+	"io"
 	"math/big"
 	"net/http"
 	"net/url"
@@ -72,6 +73,11 @@ const DEFAULT_BASE_URL = "https://api.lightspark.com/graphql/server/2023-09-13"
 func (r *Requester) ExecuteGraphql(query string, variables map[string]interface{},
 	signingKey SigningKey,
 ) (map[string]interface{}, error) {
+	return r.ExecuteGraphqlWithContext(context.Background(), query, variables, signingKey)
+}
+func (r *Requester) ExecuteGraphqlWithContext(ctx context.Context, query string, variables map[string]interface{},
+	signingKey SigningKey,
+) (map[string]interface{}, error) {
 	re := regexp.MustCompile(`(?i)\s*(?:query|mutation)\s+(?P<OperationName>\w+)`)
 	matches := re.FindStringSubmatch(query)
 	index := re.SubexpIndex("OperationName")
@@ -117,7 +123,11 @@ func (r *Requester) ExecuteGraphql(query string, variables map[string]interface{
 		return nil, err
 	}
 
-	request, err := http.NewRequest("POST", serverUrl, bytes.NewBuffer(encodedPayload))
+	request, err := http.NewRequestWithContext(ctx, http.MethodPost, serverUrl, bytes.NewBuffer(encodedPayload))
+	if err != nil {
+		return nil, err
+	}
+
 	request.SetBasicAuth(r.ApiTokenClientId, r.ApiTokenClientSecret)
 	request.Header.Add("Content-Type", "application/json")
 	request.Header.Add("X-GraphQL-Operation", operationName)
@@ -133,6 +143,9 @@ func (r *Requester) ExecuteGraphql(query string, variables map[string]interface{
 			"v":         1,
 			"signature": base64.StdEncoding.EncodeToString(signature),
 		})
+		if err != nil {
+			return nil, err
+		}
 		request.Header.Add("X-Lightspark-Signing", bytes.NewBuffer(signaturePayloadBytes).String())
 	}
 
@@ -149,7 +162,11 @@ func (r *Requester) ExecuteGraphql(query string, variables map[string]interface{
 		return nil, errors.New("lightspark request failed: " + response.Status)
 	}
 
-	data, err := ioutil.ReadAll(response.Body)
+	data, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
 	var result map[string]interface{}
 	err = json.Unmarshal(data, &result)
 	if err != nil {

lightsparkdev/go-sdk

requester/requester.go
@@ -3,6 +3,7 @@ package requester
 
 import (
 	"bytes"
+	"context"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/json"
@@ -108,6 +109,11 @@ const DEFAULT_BASE_URL = "https://api.lightspark.com/graphql/server/2023-09-13"
 func (r *Requester) ExecuteGraphql(query string, variables map[string]interface{},
 	signingKey SigningKey,
 ) (map[string]interface{}, error) {
+	return r.ExecuteGraphqlWithContext(context.Background(), query, variables, signingKey)
+}
+func (r *Requester) ExecuteGraphqlWithContext(ctx context.Context, query string, variables map[string]interface{},
+	signingKey SigningKey,
+) (map[string]interface{}, error) {
 	re := regexp.MustCompile(`(?i)\s*(?:query|mutation)\s+(?P<OperationName>\w+)`)
 	matches := re.FindStringSubmatch(query)
 	index := re.SubexpIndex("OperationName")
@@ -163,10 +169,11 @@ func (r *Requester) ExecuteGraphql(query string, variables map[string]interface{
 		return nil, err
 	}
 
-	request, err := http.NewRequest("POST", serverUrl, bytes.NewBuffer(body))
+	request, err := http.NewRequestWithContext(ctx, http.MethodPost, serverUrl, bytes.NewBuffer(body))
 	if err != nil {
 		return nil, err
 	}
+
 	request.SetBasicAuth(r.ApiTokenClientId, r.ApiTokenClientSecret)
 	request.Header.Add("Content-Type", "application/json")
 	if compressed {