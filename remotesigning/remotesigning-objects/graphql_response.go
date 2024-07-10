a91e05111aca2658fec7fccbd357f1355029b244
Zhen Lu <zhenlu@lightspark.com>
2024-07-10 09:41:42 -0700

    Revert "Create a new go mod for remote signing requests and responses. (#121)"
    This reverts commit 1c6d13168e583a5c523d15b2b24140725e135e1c.

--- remotesigning/remotesigning-objects/graphql_response.go
+++
@@ -1,8 +0,0 @@
-// Copyright ©, 2023-present, Lightspark Group, Inc. - All Rights Reserved
-package objects
-
-type GraphQLResponse struct {
-	Query       string
-	Variables   map[string]interface{}
-	OutputField string
-}
