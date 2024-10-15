93634d48a09c5708cd19a75e88dfb1d9190d2cb9
Jeremy Klein <jklein24@gmail.com>
2024-10-15 14:48:14 -0700

    Move to gin stuff

--- examples/uma-server/uma_auth_adapter.go
+++ examples/uma-server/uma_auth_adapter.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
@@ -18,49 +17,56 @@ type UmaAuthAdapter struct {
 	userService UserService
 }
 
-func (a *UmaAuthAdapter) ExecuteQuote(ctx context.Context, paymentHash string, request umaauth.ExecuteQuoteRequest) (umaauth.ImplResponse, error) {
+func (a *UmaAuthAdapter) ExecuteQuote(context *gin.Context, paymentHash string, request umaauth.ExecuteQuoteRequest) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (a *UmaAuthAdapter) FetchQuoteForLud16(ctx context.Context, sendingCurrencyCode string, receivingCurrencyCode string, lockedCurrencyAmount int64, lockedCurrencySide umaauth.LockedCurrencySide, receiverAddress string) (umaauth.ImplResponse, error) {
+func (a *UmaAuthAdapter) FetchQuoteForLud16(context *gin.Context, sendingCurrencyCode string, receivingCurrencyCode string, lockedCurrencyAmount int64, lockedCurrencySide umaauth.LockedCurrencySide, receiverAddress string) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (a *UmaAuthAdapter) GetBalance(ctx context.Context, currencyCode string) (umaauth.ImplResponse, error) {
+func (a *UmaAuthAdapter) GetBalance(context *gin.Context) {
+	currencyCode := context.Query("currency_code")
 	if currencyCode != "" && currencyCode != "SAT" {
-		return umaauth.ImplResponse{Code: 400}, fmt.Errorf("unsupported currency code: %s", currencyCode)
+		context.JSON(400, gin.H{"error": fmt.Sprintf("unsupported currency code: %s", currencyCode)})
+		return
 	}
 	// TODO: If we had multiple users and wanted to get the balance for a specific user, we would need to tweak this to
 	// only read the active user's balance.
 	node, err := GetNode(a.client, a.config.NodeUUID)
 	if err != nil {
-		return umaauth.ImplResponse{Code: 500}, fmt.Errorf("error getting balance: %s", err)
+		context.JSON(500, gin.H{"error": fmt.Sprintf("error getting node: %s", err)})
+		return
 	}
 
 	msats, err := utils.ValueMilliSatoshi((*node).GetBalances().OwnedBalance)
 	if err != nil {
-		return umaauth.ImplResponse{Code: 500}, fmt.Errorf("error getting balance: %s", err)
+		context.JSON(500, gin.H{"error": fmt.Sprintf("error getting balance: %s", err)})
+		return
 	}
 
-	return umaauth.ImplResponse{
-		Code: 200,
-		Body: &umaauth.GetBalanceResponse{
-			Balance: float32(msats),
-		},
-	}, nil
+	context.JSON(200, &umaauth.GetBalanceResponse{
+		Balance: float32(msats),
+	})
 }
 
-func (a *UmaAuthAdapter) GetBudgetEstimate(ctx context.Context, sendingCurrencyCode string, sendingCurrencyAmount int64, budgetCurrencyCode string) (umaauth.ImplResponse, error) {
+func (a *UmaAuthAdapter) GetBudgetEstimate(context *gin.Context, sendingCurrencyCode string, sendingCurrencyAmount int64, budgetCurrencyCode string) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (a *UmaAuthAdapter) GetInfo(ctx context.Context) (umaauth.ImplResponse, error) {
+func (a *UmaAuthAdapter) GetInfo(context *gin.Context) {
+	user, err := a.userService.GetUserFromContext(context)
+	if err != nil {
+		context.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
 	node, err := GetNode(a.client, a.config.NodeUUID)
 	if err != nil {
-		return umaauth.ImplResponse{Code: 500}, fmt.Errorf("error getting node: %s", err)
+		context.JSON(500, gin.H{"error": fmt.Sprintf("error getting node: %s", err)})
+		return
 	}
 
 	network := "mainnet"
@@ -82,66 +88,61 @@ func (a *UmaAuthAdapter) GetInfo(ctx context.Context) (umaauth.ImplResponse, err
 		color = new(string)
 	}
 
-	return umaauth.ImplResponse{
-		Code: 200,
-		Body: &umaauth.GetInfoResponse{
-			Alias:   "Golang Demo VASP",
-			Color:   *color,
-			Pubkey:  *pubkey,
-			Network: network,
-			Methods: a.config.SupportedNwcCommands,
-			Lud16:
-		},
-	}, nil
+	context.JSON(200, &umaauth.GetInfoResponse{
+		Alias:   "Golang Demo VASP",
+		Color:   *color,
+		Pubkey:  *pubkey,
+		Network: network,
+		Methods: a.config.SupportedNwcCommands,
+		Lud16:   user.GetUmaAddress(a.config, context),
+	})
 }
 
-func (a *UmaAuthAdapter) ListTransactions(ctx context.Context, from int64, until int64, limit int32, offset int32, unpaid bool, transactionType umaauth.TransactionType) (umaauth.ImplResponse, error) {
+func (a *UmaAuthAdapter) ListTransactions(context *gin.Context, from int64, until int64, limit int32, offset int32, unpaid bool, transactionType umaauth.TransactionType) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (a *UmaAuthAdapter) LookupInvoice(ctx context.Context, paymentHash string) (umaauth.ImplResponse, error) {
+func (a *UmaAuthAdapter) LookupInvoice(context *gin.Context, paymentHash string) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (a *UmaAuthAdapter) LookupUserByLud16(ctx context.Context, receiverAddress string, baseSendingCurrencyCode string) (umaauth.ImplResponse, error) {
+func (a *UmaAuthAdapter) LookupUserByLud16(context *gin.Context, receiverAddress string, baseSendingCurrencyCode string) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (a *UmaAuthAdapter) MakeInvoice(ctx context.Context, request umaauth.MakeInvoiceRequest) (umaauth.ImplResponse, error) {
+func (a *UmaAuthAdapter) MakeInvoice(context *gin.Context, request umaauth.MakeInvoiceRequest) {
 	invoiceType := objects.InvoiceTypeStandard
 	invoice, err := a.client.CreateInvoice(a.config.NodeUUID, request.Amount, &request.Description, &invoiceType, &request.Expiry)
 	if err != nil {
-		return umaauth.ImplResponse{Code: 500}, fmt.Errorf("error creating invoice: %s", err)
+		context.JSON(500, gin.H{"error": fmt.Sprintf("error creating invoice: %s", err)})
+		return
 	}
 	expiresAt := invoice.Data.ExpiresAt.Unix()
-	return umaauth.ImplResponse{
-		Code: 200,
-		Body: &umaauth.Transaction{
-			Type:        umaauth.INCOMING,
-			Amount:      request.Amount,
-			Invoice:     &invoice.Data.EncodedPaymentRequest,
-			Description: invoice.Data.Memo,
-			ExpiresAt:   &expiresAt,
-			PaymentHash: invoice.Data.PaymentHash,
-			CreatedAt:   invoice.CreatedAt.Unix(),
-		},
-	}, nil
+	context.JSON(200, &umaauth.Transaction{
+		Type:        umaauth.INCOMING,
+		Amount:      request.Amount,
+		Invoice:     &invoice.Data.EncodedPaymentRequest,
+		Description: invoice.Data.Memo,
+		ExpiresAt:   &expiresAt,
+		PaymentHash: invoice.Data.PaymentHash,
+		CreatedAt:   invoice.CreatedAt.Unix(),
+	})
 }
 
-func (a *UmaAuthAdapter) PayInvoice(ctx context.Context, request umaauth.PayInvoiceRequest) (umaauth.ImplResponse, error) {
+func (a *UmaAuthAdapter) PayInvoice(context *gin.Context, request umaauth.PayInvoiceRequest) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (a *UmaAuthAdapter) PayKeysend(ctx context.Context, request umaauth.PayKeysendRequest) (umaauth.ImplResponse, error) {
+func (a *UmaAuthAdapter) PayKeysend(context *gin.Context, request umaauth.PayKeysendRequest) {
 	//TODO implement me
 	panic("implement me")
 }
 
-func (a *UmaAuthAdapter) PayToLud16Address(ctx context.Context, request umaauth.PayToAddressRequest) (umaauth.ImplResponse, error) {
+func (a *UmaAuthAdapter) PayToLud16Address(context *gin.Context, request umaauth.PayToAddressRequest) {
 	//TODO implement me
 	panic("implement me")
 }
@@ -160,13 +161,37 @@ func (a *UmaAuthAdapter) HandleTokenExchange(context *gin.Context) string {
 }
 
 func (a *UmaAuthAdapter) RegisterRoutes(engine *gin.Engine) {
-	umaAuthController := umaauth.NewUmaAuthAPIController(a)
-	routes := umaAuthController.Routes()
-	for _, route := range routes {
-		engine.Handle(route.Method, route.Pattern, func(context *gin.Context) {
-			route.HandlerFunc(context.Writer, context.Request)
-		})
-	}
+	//umaAuthController := umaauth.NewUmaAuthAPIController(a)
+	//routes := umaAuthController.Routes()
+	//for _, route := range routes {
+	//	engine.Handle(route.Method, route.Pattern, func(context *gin.Context) {
+	//		route.HandlerFunc(context.Writer, context.Request)
+	//	})
+	//}
+
+	engine.GET("/umanwc/balance", func(context *gin.Context) {
+		a.GetBalance(context)
+	})
+
+	engine.GET("/umanwc/info", func(context *gin.Context) {
+		a.GetInfo(context)
+	})
+
+	engine.POST("/umanwc/invoice", func(context *gin.Context) {
+		body, err := context.GetRawData()
+		if err != nil {
+			context.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+		var requestBody umaauth.MakeInvoiceRequest
+		err = json.Unmarshal(body, &requestBody)
+		if err != nil {
+			context.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+
+		a.MakeInvoice(context, requestBody)
+	})
 
 	engine.POST("/umanwc/token", func(context *gin.Context) {
 		user, err := a.userService.GetUserFromContext(context)
